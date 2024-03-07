2094fad9afe730fdc289b0b4efefc2d011186072
Szymon Pilarczyk <83362@student.ue.poznan.pl>
2024-03-07 09:28:04 +0100

    Second Commit2

---
+++ sroda14/exercise1.py
@@ -0,0 +1,23 @@
+bok_a = 100.5
+pole_kw = bok_a * bok_a
+print("Pole kwadratu wynosi:" + str(pole_kw))
+
+
+bok_b = 5
+h = 13
+pole_trojkata = bok_b * h /2
+print("Pole trójkąta wynosi:" + str(pole_trojkata))
+
+
+bok_c = 7
+bok_d = 3
+hh = 5
+pole_trapezu = (bok_c + bok_d) * hh / 2
+print("Pole trapezu wynosi:" + str(pole_trapezu))
+
+
+
+bok_e = 17
+bok_f = 37
+pole_prostokata = bok_e * bok_f
+print("Pole prostokąta:" + str(pole_prostokata))
